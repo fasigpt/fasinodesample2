1f7d859cb691ffb857f763dcae32417f240c9cdc
fasigpt <fasigpt@gmail.com>
2020-05-12 09:27:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
+  try {
+ 
+  throw "TEST Exception";
+  
+}
+catch (e) {
+  console.log("Exception Found")
+  console.log(e);
+  
+}
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
